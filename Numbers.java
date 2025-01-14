29d687a0c59b30bc7405c19f09fdfcd2edd0e0e4
nehanirgude <nehanirgude3@gmail.com>
2025-01-14 18:37:32 +0530

    done

--- Numbers.java
+++ Numbers.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Numbers {
 
     // check wether a number is palindrom or not
@@ -55,6 +57,85 @@ public class Numbers {
 
     }
 
+    // check the number is prime or not
+
+    public static boolean isPrime(int n) {
+        boolean isprime = true;
+        for (int i = 2; i < n - 1; i++) {
+            if (n % i == 0) {
+                isprime = false;
+            }
+        }
+        return isprime;
+    }
+
+    // 4
+    // prime in range
+    public static void primeInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(n)) {
+                System.out.print(i + "is prime");
+            }
+
+        }
+        System.out.println();
+    }
+
+    // check if the number is armstrog or not
+    public static boolean armstrong(int number) {
+        int originalNum = number;
+        int duplicate = number;
+        int count = 0;
+        // counting the numbers of digits in number
+        while (number > 0) {
+            number = number / 10;
+            count++;
+        }
+
+        // System.out.println(count);
+        int newNum = 0;
+        int currNum = 0;
+
+        while (duplicate > 0) {
+            int lastDigit = duplicate % 10;
+
+            currNum = (int) Math.pow(lastDigit, count);
+            newNum += currNum;
+
+            duplicate = duplicate / 10;
+        }
+        if (newNum == originalNum) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public static boolean isPerfect(int number) {
+        int sum = 0;
+
+        for (int i = 1; i < number - 1; i++) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+
+        }
+        if (sum == number) {
+            return true;
+        }
+
+        return false;
+    }
+    // to check the number is odd or even
+
+    public static boolean isOddEven(int number) {
+
+        if (number % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String argsp[]) {
 
         // 1
@@ -62,9 +143,36 @@ public class Numbers {
         // System.out.println(palindrom(number));
 
         // 2
-        int min = 10;
-        int max = 50;
-        palindromInRange(min, max);
+        // int min = 10;
+        // int max = 50;
+        // palindromInRange(min, max);
+
+        // 3
+        // int n = 8;
+        // System.out.println(isPrime(n));
+
+        // 4
+        // int n = 20;
+        // primeInRange(n);
+
+        // 5
+        // int number = 153;
+        // System.out.println(armstrong(number));
+
+        // 6
+        // int number = 6;
+        // System.out.println(isPerfect(number));
+
+        // 7
+        int number = 16;
+        // System.out.println(isOddEven(number));
+
+        // boolean result = isOddEven(number);
+        // if (result == true) {
+        // System.out.println(number + "number is even");
+        // } else {
+        // System.out.println("number is odd");
+        // }
 
     }
 }
