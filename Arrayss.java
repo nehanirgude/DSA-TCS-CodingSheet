6980656f7de3a777d6c297ef8cdc8f322fb7fac3
nehanirgude <nehanirgude3@gmail.com>
2025-01-14 13:00:47 +0530

    done

--- Arrayss.java
+++ Arrayss.java
@@ -397,6 +397,26 @@ public class Arrayss {
 
     }
 
+    public static int equilibrium(int arr[]) {
+        int leftSum, rightSum;
+        for (int i = 0; i < arr.length; i++) {
+            leftSum = 0;
+            for (int j = 0; j < i; j++) {
+                leftSum += arr[j];
+            }
+            rightSum = 0;
+            for (int j = i + 1; j < arr.length; j++) {
+                rightSum += arr[j];
+            }
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+        }
+        return -1;
+
+    }
+
     public static void main(String args[]) {
 
         // 1
@@ -497,6 +517,10 @@ public class Arrayss {
         // for (int i = 0; i < arr.length; i++) {
         // System.out.print(arr[i] + " ");
         // }
+
+        // 19
+        int arr[] = { 2, 3, -1, 8, 4 };
+        System.out.println(equilibrium(arr));
 
     }
 }
