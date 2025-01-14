1be95136f60fcf07aea8ced55b6888235471eaf6
nehanirgude <nehanirgude3@gmail.com>
2025-01-14 14:17:43 +0530

    done

--- Numbers.java
+++ Numbers.java
@@ -25,12 +25,46 @@ public class Numbers {
         return false;
 
     }
+    // 2
+    // Problem Statement: Given a range of numbers, find all the palindrome numbers
+    // in the range.
+
+    public static void palindromInRange(int min, int max) {
+
+        for (int i = min; i < max; i++) {
+            int duplicate = i;
+            int number = i;
+            int reverse = 0;
+
+            while (number > 0) {
+
+                // last digit
+                int lasDigit = number % 10;
+
+                // append lastdigit in reverse num
+                reverse = (reverse * 10) + lasDigit;
+
+                // decrese the number
+                number = number / 10;
+
+            }
+            if (reverse == duplicate) {
+                System.out.println(i);
+            }
+        }
+
+    }
 
     public static void main(String argsp[]) {
 
         // 1
-        int number = 1331;
-        System.out.println(palindrom(number));
+        // int number = 1331;
+        // System.out.println(palindrom(number));
+
+        // 2
+        int min = 10;
+        int max = 50;
+        palindromInRange(min, max);
 
     }
 }
